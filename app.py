f5f38855082cbd4c9a9bd3c88bbe1b72eb96f107
chandrababu-gowda <chandrababugowda3042003@gmail.com>
2024-07-15 20:27:04 +0530

    Refactor: Changes for better hosting

--- app.py
+++ app.py
@@ -9,9 +9,6 @@ load_dotenv()
 
 app = Flask(__name__)
 
-if __name__ == "__main__":
-    app.run(debug=True,host='0.0.0.0',port=5000)
-
 @app.post("/")
 def calculate_nutrients():
     requestObj = request.get_json()
